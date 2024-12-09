88e52d13608594ce13e37f573de247c926820e1b
Gonzalo Ferreyra <gonferreyra@gmail.com>
2024-12-09 12:18:57 -0300

    config and middlewares added to server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,45 @@
 import express from 'express';
+import 'dotenv/config';
+import cors from 'cors';
+import dbConnection from './config/db';
+import { APP_ORIGIN, NODE_ENV, SERVER_PORT } from './constants/env';
+import cookieParser from 'cookie-parser';
+import errorHandler from './middleware/errorHandler';
+import authRoutes from './routes/auth.route';
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.status(200).json({
-    status: 'healthy',
-  });
-});
+const Server = async () => {
+  // JSON - reading and parsing body
+  app.use(express.json());
+  // URL - reading and parsing body
+  app.use(express.urlencoded({ extended: true }));
 
-app.listen(3001, () => {
-  console.log('Server running on port 3001');
-});
+  app.use(
+    cors({
+      origin: APP_ORIGIN,
+      credentials: true,
+    })
+  );
+  // Cookie Parser Middleware
+  app.use(cookieParser()); // parse cookies - req.cookies available
+
+  app.get('/', (req, res) => {
+    res.status(200).json({
+      status: 'healthy',
+    });
+  });
+
+  // Authentication routes
+  app.use('/auth', authRoutes);
+
+  // Error Handler Middleware
+  app.use(errorHandler);
+
+  app.listen(SERVER_PORT, async () => {
+    console.log(`Server running on port ${SERVER_PORT} in ${NODE_ENV} mode`);
+    await dbConnection();
+  });
+};
+
+Server();
