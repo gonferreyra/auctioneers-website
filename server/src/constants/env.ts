8fdd2de9616e7d2a201565a5a9f44076af422592
Gonzalo Ferreyra <gonferreyra@gmail.com>
2024-12-09 11:32:52 -0300

    added function to validate env to a string

---
+++ server/src/constants/env.ts
@@ -0,0 +1,26 @@
+const getEnv = (key: string, defaultValue?: string) => {
+  const value = process.env[key] || defaultValue;
+
+  if (value === undefined) {
+    throw new Error(`Environment variable ${key} is not defined`);
+  }
+
+  return value;
+};
+
+// ENVIROMENT
+export const NODE_ENV = getEnv('NODE_ENV');
+
+// FRONTEND URL
+export const APP_ORIGIN = getEnv('APP_ORIGIN');
+
+// SERVER
+export const SERVER_PORT = getEnv('SERVER_PORT');
+export const SERVER_HOSTNAME = getEnv('SERVER_HOSTNAME');
+
+// DB
+export const DB_NAME = getEnv('DB_NAME');
+export const DB_USER = getEnv('DB_USER');
+export const DB_PASSWORD = getEnv('DB_PASSWORD');
+export const DB_PORT = getEnv('DB_PORT');
+export const DB_HOST = getEnv('DB_HOST');
