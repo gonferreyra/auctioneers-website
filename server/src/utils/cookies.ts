da42f095813bc01ae9872de750d97240a52bf896
Gonzalo Ferreyra <gonferreyra@gmail.com>
2024-12-10 16:46:15 -0300

    created cookies function

---
+++ server/src/utils/cookies.ts
@@ -0,0 +1,31 @@
+import { CookieOptions, Response } from 'express';
+import { fifteenMinutesFromNow, thirtyDaysFromNow } from './date';
+
+const secure = process.env.NODE_ENV !== 'development';
+
+type Params = {
+  res: Response;
+  accessToken: string;
+  refreshToken: string;
+};
+
+export const setAuthenticationCookies = ({
+  res,
+  accessToken,
+  refreshToken,
+}: Params) => {
+  return res
+    .cookie('accessToken', accessToken, {
+      sameSite: 'strict',
+      httpOnly: true,
+      secure,
+      expires: fifteenMinutesFromNow(),
+    })
+    .cookie('refrshToken', refreshToken, {
+      sameSite: 'strict',
+      httpOnly: true,
+      secure,
+      expires: thirtyDaysFromNow(),
+      path: '/auth/refresh',
+    });
+};
