dd6d97d1973ea25c8a90affa9eebfd203a0744e3
Gonzalo Ferreyra <gonferreyra@gmail.com>
2024-12-18 11:26:49 -0300

    getCaseById handler created

--- server/src/routes/case.route.ts
+++ server/src/routes/case.route.ts
@@ -4,5 +4,6 @@ import * as controllers from '../controllers/case.controller';
 const caseRoutes = Router();
 
 caseRoutes.get('/', controllers.getCasesHandler);
+caseRoutes.get('/:id', controllers.getCaseByIdHandler);
 
 export default caseRoutes;
