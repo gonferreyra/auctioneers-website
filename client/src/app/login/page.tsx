c1142ecd483b0ccc1f312c02513aba2c9e236b5d
Gonzalo Ferreyra <gonferreyra@gmail.com>
2024-12-28 18:08:56 -0300

    refactor login using rhf and zod

--- client/src/app/login/page.tsx
+++ client/src/app/login/page.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import { FormEvent, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
@@ -9,53 +8,44 @@ import { Card } from '@/components/ui/card';
 import { Gavel, Loader2 } from 'lucide-react';
 import { useMutation } from '@tanstack/react-query';
 import { login } from '@/lib/api';
-import { loginSchema } from '@/validations/schemas';
+import { loginSchema, TLoginSchema } from '@/validations/schemas';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { toast } from 'sonner';
 
 export default function LoginPage() {
   const router = useRouter();
-  const [formData, setFormData] = useState({
-    email: '',
-    password: '',
-  });
-  const [errors, setErrors] = useState({ email: '', password: '' });
 
   const {
-    mutate: signIn,
-    isPending,
-    isError,
-    error,
-  } = useMutation({
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<TLoginSchema>({
+    resolver: zodResolver(loginSchema),
+  });
+
+  const { mutate: signIn, isPending } = useMutation({
     mutationFn: login,
     onSuccess: () => {
-      router.push('/dashboard');
+      router.replace('/dashboard');
     },
     onError: (error) => {
-      console.error(error);
+      toast.error(error.message);
     },
   });
 
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const result = loginSchema.safeParse(formData);
-
-    if (!result.success) {
-      const fieldErrors = result.error.format();
-      setErrors({
-        email: fieldErrors.email?._errors[0] || '',
-        password: fieldErrors.password?._errors[0] || '',
-      });
-      return;
-    }
-    setErrors({ email: '', password: '' });
-    signIn(formData);
+  const onSubmit = async (data: TLoginSchema) => {
+    signIn(data);
   };
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
-
-    // Clear the error for the field being updated
-    setErrors({ ...errors, [name]: '' });
+  // client side validation errors
+  const onError = () => {
+    (Object.keys(errors) as Array<keyof TLoginSchema>).forEach((key) => {
+      const error = errors[key];
+      if (error) {
+        toast.error(error.message || 'Hubo un error');
+      }
+    });
   };
 
   return (
@@ -71,48 +61,36 @@ export default function LoginPage() {
           </p>
         </div>
 
-        <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
+        <form
+          className="mt-8 space-y-6"
+          onSubmit={handleSubmit(onSubmit, onError)}
+          noValidate
+        >
           <div className="space-y-4">
             <div>
               <Label htmlFor="email">Email</Label>
               <Input
+                {...register('email')}
                 id="email"
                 name="email"
                 type="email"
                 required
-                value={formData.email}
-                onChange={handleChange}
                 className="mt-1"
               />
-              {errors.email && (
-                <p className="text-sm text-red-500">{errors.email}</p>
-              )}
             </div>
 
             <div>
               <Label htmlFor="password">Password</Label>
               <Input
+                {...register('password')}
                 id="password"
                 name="password"
                 type="password"
                 required
-                value={formData.password}
-                onChange={handleChange}
                 className="mt-1"
               />
-              {errors.password && (
-                <p className="text-sm text-red-500">{errors.password}</p>
-              )}
             </div>
           </div>
-          {/* Handle errors */}
-
-          {isError && (
-            <div className="rounded border border-red-500 p-2 text-center text-red-500 transition">
-              <p>{error.message}</p>
-              {/* <p>Test error</p> */}
-            </div>
-          )}
 
           <Button
             type="submit"
--- client/src/validations/schemas.ts
+++ client/src/validations/schemas.ts
@@ -1,9 +1,11 @@
 import z from 'zod';
 
 export const loginSchema = z.object({
-  email: z.string().email(),
+  email: z.string().email('Debe ingresar un email valido'),
   password: z
     .string()
     .min(6, 'La contraseña debe contener 6 caracteres')
     .max(255),
 });
+
+export type TLoginSchema = z.infer<typeof loginSchema>;
