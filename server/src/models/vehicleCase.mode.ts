9b96dc20f44d8f36bfe9c1c06f381ddd828fb3c8
Gonzalo Ferreyra <gonferreyra@gmail.com>
2025-01-02 16:41:07 -0300

    create property and vehicle dynamic models

---
+++ server/src/models/vehicleCase.mode.ts
@@ -0,0 +1,81 @@
+import {
+  DataTypes,
+  InferAttributes,
+  InferCreationAttributes,
+  Model,
+} from 'sequelize';
+import { DB } from '../config/db';
+
+interface IVehicleCaseModel
+  extends Model<
+    InferAttributes<IVehicleCaseModel>,
+    InferCreationAttributes<IVehicleCaseModel>
+  > {
+  id?: number;
+  caseId: number;
+  licensePlate?: string;
+  brand?: string;
+  model?: string;
+  year?: number;
+  chassisBrand?: string;
+  chassisNumber?: string;
+  engineBrand?: string;
+  engineNumber?: string;
+}
+
+const VehicleCaseModel = DB.define<IVehicleCaseModel>(
+  'VehicleCase',
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    caseId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'cases',
+        key: 'id',
+      },
+    },
+    licensePlate: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    brand: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    model: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    year: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    chassisBrand: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    chassisNumber: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    engineBrand: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    engineNumber: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+  },
+  {
+    tableName: 'vehicle_case',
+    timestamps: true,
+  }
+);
+
+export default VehicleCaseModel;
