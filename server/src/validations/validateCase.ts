6cb051ee8958fe16a18b7d4145b2be5745c2a725
Gonzalo Ferreyra <gonferreyra@gmail.com>
2025-01-07 17:28:50 -0300

    modify validateCase function and add a validateUpdateCase function

--- server/src/validations/validateCase.ts
+++ server/src/validations/validateCase.ts
@@ -1,4 +1,6 @@
+import CustomError from '../utils/customError';
 import {
+  appraisalCaseSchema,
   baseCaseSchema,
   propertyCaseSchema,
   vehicleCaseSchema,
@@ -17,10 +19,10 @@ export const validateCase = (data: any) => {
       specificData = propertyCaseSchema.parse(data.specificData);
       break;
     case 'appraisal':
-      specificData = {};
+      specificData = appraisalCaseSchema.parse(data.specificData);
       break;
     default:
-      throw new Error('Invalid case type');
+      throw new CustomError(400, 'Invalid case type');
   }
 
   return {
@@ -28,3 +30,59 @@ export const validateCase = (data: any) => {
     specificData,
   };
 };
+
+export const validateUpdateCase = (data: any) => {
+  const baseData = baseCaseSchema.partial().parse(data);
+
+  let specificData;
+
+  switch (baseData.caseType) {
+    case 'vehicle':
+      specificData = vehicleCaseSchema.partial().parse(data.specificData);
+      break;
+    case 'property':
+      specificData = propertyCaseSchema.partial().parse(data.specificData);
+      break;
+    case 'appraisal':
+      specificData = appraisalCaseSchema.partial().parse(data.specificData);
+      break;
+    default:
+      throw new CustomError(400, 'Invalid case type');
+  }
+
+  return {
+    ...baseData,
+    specificData,
+  };
+};
+
+// export const validateCase = (data: any, options = { partial: false }) => {
+//   const baseData = options.partial
+//     ? baseCaseSchema.partial().parse(data)
+//     : baseCaseSchema.parse(data);
+
+//   let specificData;
+
+//   switch (baseData.caseType) {
+//     case 'vehicle':
+//       specificData = options.partial
+//         ? vehicleCaseSchema.partial().parse(data.specificData)
+//         : vehicleCaseSchema.parse(data.specificData);
+//       break;
+//     case 'property':
+//       specificData = options.partial
+//         ? propertyCaseSchema.parse(data.specificData)
+//         : propertyCaseSchema.parse(data.specificData);
+//       break;
+//     case 'appraisal':
+//       specificData = {};
+//       break;
+//     default:
+//       throw new Error('Invalid case type');
+//   }
+
+//   return {
+//     ...baseData,
+//     specificData,
+//   };
+// };
