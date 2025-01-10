b5962539c3767df5312ec7e830cdc573cc47b99c
Gonzalo Ferreyra <gonferreyra@gmail.com>
2025-01-10 15:17:57 -0300

    update case type

--- client/src/types/case.ts
+++ client/src/types/case.ts
@@ -1,44 +1,59 @@
-// export interface Case {
-//   id: string;
-//   internalNumber: string; // Case number for organization inside auctioneers
-//   recordNumber: string; // Official case record number
-//   title: string; // e.g., "Banco suquia c/ Daniel blanco - Ejecucion hipotecaria"
-//   status: 'active' | 'paralyzed' | 'closed' | 'pending';
-//   court: string; // e.g., "Juzgado 1ra Instancia y 11 nominacion"
-//   plaintiff: string; // Main plaintiff
-//   defendant: string; // Main defendant
-//   type: string; // Type of case (e.g., "Ejecucion hipotecaria")
-//   filingDate: string;
-//   notes?: string;
-//   movements: CaseMovement[];
-// }
+export type CaseStatus = 'active' | 'paralyzed' | 'closed';
+export type CaseType = 'vehicle' | 'property' | 'appraisal';
 
-export interface Case {
-  id: number;
-  intern_number: string;
-  status: 'active' | 'paralyzed' | 'closed';
-  record: string; // expte
-  plaintiff: string; // actor
-  defendant: string; // demandado
-  type: string; // tipo de juicio
-  court: string; // juzgado
-  law_office?: string; // estudio
-  debt?: number; // deuda
-  aps?: Date; // fecha preventiva de subasta
-  aps_expiresAt?: Date; // fecha caducidad preventiva
-  is_executed: string; // que se ejecuta (matricula tanto, auto dominio tanto)
-  address?: string; // domicilio
-  account_dgr?: string; // cuenta dgr
-  nomenclature?: string; // nomenclatura
-  description?: string; // descripcion de lo que se ejecuta (matricula completa)
-  createdAt: Date;
-  updatedAt: Date;
+interface BaseCase {
+  id: string;
+  internNumber: string;
+  status: CaseStatus;
+  record: string;
+  plaintiff: string;
+  defendant: string;
+  type: string;
+  court: string;
+  lawOffice: string;
+  debt: number;
+  caseType: CaseType;
+  movements: CaseMovement[];
+  vehicleDetails?: VehicleCase;
+  propertyDetails?: PropertyCase;
+  appraisalDetails?: AppraisalCase;
 }
 
+interface VehicleCase extends BaseCase {
+  // caseType: 'vehicle';
+  licensePlate: string;
+  brand: string;
+  model: string;
+  year: number;
+  chassisBrand: string;
+  chassisNumber: string;
+  engineBrand: string;
+  engineNumber: string;
+}
+
+interface PropertyCase extends BaseCase {
+  // caseType: 'property';
+  propertyRegistration: string;
+  percentage: number;
+  address: string;
+  description: string;
+  aps: Date;
+  apsExpiresAt: Date;
+  accountDgr: string;
+  nomenclature: string;
+}
+
+interface AppraisalCase extends BaseCase {
+  // caseType: 'appraisal';
+  itemsToAppraise: string[];
+  description: string;
+}
+
+export type Case = VehicleCase | PropertyCase | AppraisalCase;
+
 export interface CaseMovement {
-  id: number;
-  // case_id: number;
+  id: string;
+  caseInternNumber: string;
   description: string;
   createdAt: Date;
-  updatedAt: Date;
 }
