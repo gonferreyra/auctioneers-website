a4d801fd95fc2199f6b6050e42c9643a731c2f5a
Gonzalo Ferreyra <gonferreyra@gmail.com>
2025-01-14 12:09:11 -0300

    add loading state to debounce hook

--- client/src/lib/hooks.ts
+++ client/src/lib/hooks.ts
@@ -2,10 +2,13 @@ import { useEffect, useState } from 'react';
 
 export const useDebounce = (value: string, delay: number) => {
   const [debouncedValue, setDebouncedValue] = useState('');
+  const [isDebounceLoading, setIsDebounceLoading] = useState(false);
 
   useEffect(() => {
+    setIsDebounceLoading(true);
     const timer = setTimeout(() => {
       setDebouncedValue(value);
+      setIsDebounceLoading(false);
     }, delay);
 
     return () => {
@@ -13,5 +16,5 @@ export const useDebounce = (value: string, delay: number) => {
     };
   }, [value]);
 
-  return { debouncedValue };
+  return { debouncedValue, isDebounceLoading };
 };
