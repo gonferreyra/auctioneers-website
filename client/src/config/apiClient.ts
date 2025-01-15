c4622241d775f7f385a94cf2d34cf81ff6ffa9d5
Gonzalo Ferreyra <gonferreyra@gmail.com>
2025-01-15 16:28:57 -0300

    modify the interceptor to change the properties of the API response

--- client/src/config/apiClient.ts
+++ client/src/config/apiClient.ts
@@ -5,8 +5,38 @@ const API = Axios.create({
   withCredentials: true,
 });
 
+// Función para transformar los datos
+function transformData(data: any): any {
+  if (Array.isArray(data)) {
+    return data.map(transformData); // If it's an array, transform every element
+  }
+
+  if (data.vehicleDetails) {
+    data.specificData = data.vehicleDetails;
+    delete data.vehicleDetails;
+  }
+
+  if (data.propertyDetails) {
+    data.specificData = data.propertyDetails;
+    delete data.propertyDetails;
+  }
+
+  if (data.appraisalDetails) {
+    data.specificData = data.appraisalDetails;
+    delete data.appraisalDetails;
+  }
+
+  return data;
+}
+
 API.interceptors.response.use(
-  (response) => response,
+  (response) => {
+    // console.log('response', response.data.cases);
+    if (response.data) {
+      response.data.cases = transformData(response.data.cases);
+    }
+    return response;
+  },
   (error) => {
     const { status, data } = error.response;
     // return Promise.reject({ status, ...data });
