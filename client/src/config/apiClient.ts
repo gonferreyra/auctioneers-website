bce1ba2f44fbb37d05f81695bf35743700531406
Gonzalo Ferreyra <gonferreyra@gmail.com>
2025-01-24 16:08:51 -0300

    delete transformData funcion and send response.data directly

--- client/src/config/apiClient.ts
+++ client/src/config/apiClient.ts
@@ -5,36 +5,9 @@ const API = Axios.create({
   withCredentials: true,
 });
 
-// Función para transformar los datos
-function transformData(data: any): any {
-  if (Array.isArray(data)) {
-    return data.map(transformData); // If it's an array, transform every element
-  }
-
-  if (data.vehicleDetails) {
-    data.specificData = data.vehicleDetails;
-    delete data.vehicleDetails;
-  }
-
-  if (data.propertyDetails) {
-    data.specificData = data.propertyDetails;
-    delete data.propertyDetails;
-  }
-
-  if (data.appraisalDetails) {
-    data.specificData = data.appraisalDetails;
-    delete data.appraisalDetails;
-  }
-
-  return data;
-}
-
 API.interceptors.response.use(
   (response) => {
-    // console.log('response', response.data.cases);
-    if (response.data) {
-      response.data.cases = transformData(response.data.cases);
-    }
+    // console.log(response.data);
     return response;
   },
   (error) => {
