daebe7c64b010491ce60248cd8a71e642bdc688e
Gonzalo Ferreyra <gonferreyra@gmail.com>
2025-01-27 16:33:18 -0300

    send utility function transformData to utils

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -1,6 +1,29 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function transformData(data) {
+  if (Array.isArray(data)) {
+    return data.map(transformData); // If it's an array, transform every element
+  }
+
+  if (data.vehicleDetails) {
+    data.specificData = data.vehicleDetails;
+    delete data.vehicleDetails;
+  }
+
+  if (data.propertyDetails) {
+    data.specificData = data.propertyDetails;
+    delete data.propertyDetails;
+  }
+
+  if (data.appraisalDetails) {
+    data.specificData = data.appraisalDetails;
+    delete data.appraisalDetails;
+  }
+
+  return data;
 }
