f049bb6d71c0ef296d0d847b072f166ff7cb9690
Gonzalo Ferreyra <gonferreyra@gmail.com>
2025-01-28 11:23:59 -0300

    modify transformData function

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -5,22 +5,23 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
+// make type for data
 export function transformData(data) {
   if (Array.isArray(data)) {
     return data.map(transformData); // If it's an array, transform every element
   }
 
-  if (data.vehicleDetails) {
+  if (data?.vehicleDetails) {
     data.specificData = data.vehicleDetails;
     delete data.vehicleDetails;
   }
 
-  if (data.propertyDetails) {
+  if (data?.propertyDetails) {
     data.specificData = data.propertyDetails;
     delete data.propertyDetails;
   }
 
-  if (data.appraisalDetails) {
+  if (data?.appraisalDetails) {
     data.specificData = data.appraisalDetails;
     delete data.appraisalDetails;
   }
