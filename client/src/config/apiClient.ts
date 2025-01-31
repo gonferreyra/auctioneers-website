e12a5cb97c571dae2edf7fb7810fe4f1ef79ddbc
Gonzalo Ferreyra <gonferreyra@gmail.com>
2025-01-31 17:20:14 -0300

    implement refresh accessToken if refreshToken is available

--- client/src/config/apiClient.ts
+++ client/src/config/apiClient.ts
@@ -1,10 +1,16 @@
+import { queryClient } from '@/components/react-query-provider';
 import { transformData } from '@/lib/utils';
-import Axios from 'axios';
+import axios from 'axios';
 
-const API = Axios.create({
+const options = {
   baseURL: process.env.NEXT_PUBLIC_API_URL,
   withCredentials: true,
-});
+};
+
+const API = axios.create(options);
+
+const TokenRefreshClient = axios.create(options);
+TokenRefreshClient.interceptors.response.use((response) => response);
 
 API.interceptors.response.use(
   (response) => {
@@ -22,8 +28,21 @@ API.interceptors.response.use(
     // console.log(response.data);
     return response;
   },
-  (error) => {
-    const { status, data } = error.response;
+  async (error) => {
+    const { config, response } = error;
+    const { status, data } = response || {};
+
+    // try to refresh access token behind the scene
+    if (status === 401 && data?.errorCode === 'INVALID_ACCESS_TOKEN') {
+      try {
+        await TokenRefreshClient.get('/auth/refresh');
+        return TokenRefreshClient(config);
+      } catch (error) {
+        console.error(error);
+        queryClient.clear();
+      }
+    }
+
     // return Promise.reject({ status, ...data });
     return Promise.reject({
       status,
