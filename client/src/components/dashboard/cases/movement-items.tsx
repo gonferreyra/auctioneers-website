e375eea27261a1633424c925c20fda747343b69b
Gonzalo Ferreyra <gonferreyra@gmail.com>
2025-02-17 17:31:11 -0300

    implemented deleteMovement. Add zod validation to updateMovement

--- client/src/components/dashboard/cases/movement-items.tsx
+++ client/src/components/dashboard/cases/movement-items.tsx
@@ -3,12 +3,12 @@
 import { useState } from 'react';
 import { Button } from '@/components/ui/button';
 import { Textarea } from '@/components/ui/textarea';
-import { Calendar, Pencil, Check, X, Loader2 } from 'lucide-react';
+import { Calendar, Pencil, Check, X, Loader2, Trash2 } from 'lucide-react';
 import type { Case, CaseMovement } from '@/types/case';
 import { useForm } from 'react-hook-form';
-import type { TCreateMovementSchema } from '@/validations/schemas';
+import type { TUpdateMovementSchema } from '@/validations/schemas';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
-import { updateMovement } from '@/lib/api';
+import { deleteMovement, updateMovement } from '@/lib/api';
 import { toast } from 'sonner';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { updateMovementSchema } from '../../../validations/schemas';
@@ -17,26 +17,32 @@ import moment from 'moment';
 interface MovementItemProps {
   movement: CaseMovement;
   caseId: number;
+  caseInternNumber: string;
 }
 
-export default function MovementItem({ movement, caseId }: MovementItemProps) {
+export default function MovementItem({
+  movement,
+  caseId,
+  caseInternNumber,
+}: MovementItemProps) {
   const [isEditing, setIsEditing] = useState(false);
   const [originalMovement] = useState(movement.description);
   const queryClient = useQueryClient();
 
-  const { register, handleSubmit, getValues, setValue } =
-    useForm<TCreateMovementSchema>({
-      defaultValues: movement,
-      resolver: zodResolver(updateMovementSchema),
-    });
+  const {
+    register,
+    handleSubmit,
+    getValues,
+    setValue,
+    formState: { errors },
+  } = useForm<TUpdateMovementSchema>({
+    defaultValues: movement,
+    resolver: zodResolver(updateMovementSchema),
+  });
 
   const { mutate: updateMovementMutation, isPending } = useMutation({
     mutationFn: () =>
-      updateMovement(
-        movement.id,
-        movement.caseInternNumber,
-        getValues().description,
-      ),
+      updateMovement(movement.id, caseInternNumber, getValues().description),
     onSuccess: () => {
       setIsEditing(false);
 
@@ -61,6 +67,34 @@ export default function MovementItem({ movement, caseId }: MovementItemProps) {
       toast.error(error.message);
     },
   });
+
+  const { mutate: handleDeleteMovement, isPending: isDeletingMovement } =
+    useMutation({
+      mutationFn: () => deleteMovement(movement.id),
+      onSuccess: () => {
+        toast.success('Movimiento eliminado correctamente');
+        queryClient.setQueryData(['case', caseId], (caseData: Case) => {
+          return {
+            ...caseData,
+            movements: caseData?.movements?.filter((m) => m.id !== movement.id),
+          };
+        });
+      },
+      onError: (error) => {
+        toast.error(error.message || 'Hubo un error al eliminar el movimiento');
+      },
+    });
+
+  // client side validation errors - client errors
+  const onError = () => {
+    (Object.keys(errors) as Array<keyof TUpdateMovementSchema>).forEach(
+      (key) => {
+        const fieldName = key;
+        const errorMessage = errors[key]?.message;
+        toast.error(`${fieldName}: ${errorMessage}`);
+      },
+    );
+  };
 
   const handleCancel = () => {
     setValue('description', originalMovement);
@@ -88,13 +122,19 @@ export default function MovementItem({ movement, caseId }: MovementItemProps) {
               className="resize-none"
             />
             <div className="flex justify-end gap-2">
-              <Button size="sm" variant="ghost" onClick={handleCancel}>
+              <Button
+                size="sm"
+                variant="ghost"
+                type="button"
+                onClick={handleCancel}
+              >
                 <X className="mr-1 h-4 w-4" />
                 Cancel
               </Button>
               <Button
+                type="button"
                 size="sm"
-                onClick={handleSubmit(onSubmit)}
+                onClick={handleSubmit(onSubmit, onError)}
                 disabled={isPending}
               >
                 {isPending ? (
@@ -109,15 +149,29 @@ export default function MovementItem({ movement, caseId }: MovementItemProps) {
             </div>
           </div>
         ) : (
-          <div className="flex items-start justify-between gap-4">
+          <div className="flex items-center justify-between gap-4">
             <p className="flex-1 text-sm">{movement.description}</p>
             <Button
               size="sm"
               variant="ghost"
+              type="button"
               onClick={() => setIsEditing(true)}
-              className="mt-[-4px]"
+              disabled={!movement.id}
             >
               <Pencil className="h-4 w-4" />
+            </Button>
+            <Button
+              type="button"
+              size="sm"
+              variant="destructive"
+              onClick={() => handleDeleteMovement()}
+              disabled={isDeletingMovement || !movement.id}
+            >
+              {isDeletingMovement ? (
+                <Loader2 className="h-4 w-4 animate-spin" />
+              ) : (
+                <Trash2 className="h-4 w-4" />
+              )}
             </Button>
           </div>
         )}
